8feab9f11b00385786afcf2ea595d6f708417735
Vairamuthu <mvairamuthu2003@gmail.com>
2025-10-25 17:47:08 +0530

    feat: remove unused components and assets to clean up the project

--- pylinks-dashboard/src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Card({ children }: { children: ReactNode }) {
-  return (
-    <div className="bg-white shadow-sm rounded-xl border border-gray-100 p-6">
-      {children}
-    </div>
-  );
-}
